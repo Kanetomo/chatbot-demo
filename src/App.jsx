545bb4ff0b640dd3e59a91ae82ee44945b4d04bf
mac <mac@macnoMacBook-ea.local>
2020-12-03 11:12:10 +0900

    classComponent

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { InsertInvitation } from '@material-ui/icons';
+import React from 'react';
+import defaultDataset from './dataset';
+import './assets/styles/style.css'
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        answers: [],
+        chats: [],
+        currentId: "init",
+        dataset: defaultDataset,
+        open: false
+    }
+  }
+  render(){
+    return (
+    
+      <section className="c-section">
+        <div className="c-box">
+          {this.state.currentId}
+        </div>
+      </section>
+
+    );
+  }
+}
+
